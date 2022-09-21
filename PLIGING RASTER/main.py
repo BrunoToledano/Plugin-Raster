6d5dcc1a857ff2df09f67b31f230bc61c1f5113c
Bruno Toledano <brunoalexis@hotmail.com>
2022-09-21 08:30:11 -0500

    Plugin_Raster

---
+++ PLIGING RASTER/main.py
@@ -0,0 +1,43 @@
+import os 
+import sys
+
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+from qgis.core import *
+from qgis.gui import *
+from qgis.utils import *
+
+from .raster import interfaz
+class mainMenu:
+    def __init__(self,iface):
+        self.iface = iface
+
+    def initGui(self):
+        self.IMenu = QMenu(self.iface.mainWindow())
+        self.IMenu.setTitle("Raster")
+        self.IMenuBar = self.iface.mainWindow().menuBar()
+        self.IMenuBar.insertMenu(self.iface.firstRightStandardMenu().menuAction(), self.IMenu)
+        self.IMenuBar = self.iface.addToolBar("Raster")
+
+        self.ejemploRaster = QAction(QIcon(""),"Datos", self.iface.mainWindow())
+        self.IMenu.addAction(self.ejemploRaster)
+        self.ejemploRaster.triggered.connect(self.startInterfaz)
+
+    def startInterfaz(self):
+        self.dialogo = interfaz()
+        self.dialogo.show()
+        layers = QgsProject.instance().mapLayers().values()
+        for layer in layers:
+            if layer.type() == QgsMapLayer.VectorLayer and layer.geometryType() == QgsWkbTypes.PolygonGeometry:
+                vLayer = layer
+            if layer.type()==QgsRasterLayer.RasterLayer:
+                rLayer = layer
+                self.dialogo.ui.comboBox.addItem(rLayer.name())
+                epsg = rLayer.crs()
+                self.dialogo.ui.label.setText(self,epsg)
+         
+    def unload(self):
+        self.IMenu.DeleteLater()
+            
